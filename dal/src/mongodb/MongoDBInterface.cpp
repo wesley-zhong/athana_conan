8b72ab52b9501b15a789fa5c28abc1bc0eae283a
wesley-zhong <wiqi.zhong@gmail.com>
2025-11-23 20:07:41 +0800

    update

--- dal/src/mongodb/MongoDBInterface.cpp
+++ dal/src/mongodb/MongoDBInterface.cpp
@@ -1,7 +1,7 @@
 //
 // Created by Wesly Zhong on 2025/11/23.
 //
-
+#include "core/log/XLog.h"
 #include "MongoDBInterface.h"
 
 int MongoDBInterface::testInit() {
@@ -19,8 +19,16 @@ int MongoDBInterface::testInit() {
         auto admin = client["admin"];
         auto command = make_document(kvp("ping", 1));
         auto result = admin.run_command(command.view());
-        std::cout << bsoncxx::to_json(result) << "\n";
-        std::cout << "Pinged your deployment. You successfully connected to MongoDB!\n";
+        INFO_LOG( bsoncxx::to_json(result));
+        INFO_LOG( "Pinged your deployment. You successfully connected to MongoDB!\n");
+
+        auto  gameDB =  client["game"];
+        auto roleCollection = gameDB["role"];
+        auto opt =  roleCollection.find_one(make_document(kvp("_id", 1000001)));
+        auto value =  opt.value();
+        INFO_LOG("uuuuu value = {}",  bsoncxx::to_json(value));
+
+
     }
     catch (const mongocxx::exception &e)
     {
