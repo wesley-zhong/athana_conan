056e1d3a657b77d897aeeb23c14fa343e5acf4f6
zhongweiqi <zhongweiqi@yhkt.com>
2025-11-27 09:32:08 +0800

    update

--- build_linux.sh
+++ build_linux.sh
@@ -1,3 +1,50 @@
-conan install . --output-folder=cmake-build-debug --build=missing -s build_type=Debug -s compiler.cppstd=17
-cmake -B  cmake-build-debug -S . -DCMAKE_TOOLCHAIN_FILE=cmake-build-debug/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Debug
-cmake --build cmake-build-debug -j8
+#!/bin/bash
+
+set -e
+
+BUILD_DIR="cmake-build-debug"
+TOOLCHAIN_FILE="${BUILD_DIR}/conan_toolchain.cmake"
+CPPSTD=17
+JOBS=8
+
+function do_install() {
+    echo "==== 1. Running Conan install ===="
+    conan install . \
+        --output-folder="${BUILD_DIR}" \
+        --build=missing \
+        -s build_type=Debug \
+        -s compiler.cppstd=${CPPSTD}
+}
+
+function do_configure() {
+    echo "==== 2. Running CMake configure ===="
+    cmake -B "${BUILD_DIR}" -S . \
+        -DCMAKE_TOOLCHAIN_FILE="${TOOLCHAIN_FILE}" \
+        -DCMAKE_BUILD_TYPE=Debug
+}
+
+function do_build() {
+    echo "==== 3. Running CMake build ===="
+    cmake --build "${BUILD_DIR}" -j${JOBS}
+}
+
+case "$1" in
+    install)
+        do_install
+        ;;
+    configure)
+        do_configure
+        ;;
+    build)
+        do_build
+        ;;
+    all)
+        do_install
+        do_configure
+        do_build
+        ;;
+    *)
+        echo "Usage: $0 {install|configure|build|all}"
+        exit 1
+        ;;
+esac
