c3a7721d9f79d7b826ec74c93c9dcc9f71fec2cf
wesley <wiqi.zhong@gmail.com>
2026-09-14 11:03:49 +0800

    update

---
+++ game/code-gen/QuestDO_bson.gen.cpp
@@ -0,0 +1,25 @@
+#include "QuestDO.hpp"
+#include <bsoncxx/builder/stream/document.hpp>
+#include <bsoncxx/types.hpp>
+
+bsoncxx::document::value QuestDO::toBson() const {
+    bsoncxx::builder::stream::document doc{};
+    {
+        auto __view = BsonSerializable::toBson().view();
+        for (auto&& e : __view) {
+            doc << e.key() << e.get_value();
+        }
+    }
+    doc << "_id" << _id;
+ //   doc << "questIdsFinished" << questIdsFinished;
+   // doc << "curQuestIds" << curQuestIds;
+    return doc << bsoncxx::builder::stream::finalize;
+}
+
+void QuestDO::fromBson(bsoncxx::document::view v) {
+    BsonSerializable::fromBson(v);
+    if (auto e = v["_id"])
+        _id = e.get_int64();
+    // unsupported field type: std::vector<int64_t> questIdsFinished
+    // unsupported field type: std::vector<int64_t> curQuestIds
+}
